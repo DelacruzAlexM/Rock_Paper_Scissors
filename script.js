d010a67d69ad4223f341e48050c3a588c5b39e7c
DelacruzAlexM <117811408+DelacruzAlexM@users.noreply.github.com>
2023-02-09 01:59:18 -0800

    added playGame function and code to the game func

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let PlayerPoint = 0;
 let ComputerPoint = 0;
+let gameRound = 0;
+
 //get user input
 function getPlayerChoice(){
     let choice = window.prompt("Rock, Paper, or Scissors?");
@@ -10,47 +12,63 @@ function getPlayerChoice(){
 //randomly returns computers move rock, paper, or scissors
 function getComputerChoice(){
     let computerChoice = ["rock", "paper", "scissors"];
-    computerChoice.random();
-    return computerChoice;
+    return computerChoice[Math.floor(Math.random()*computerChoice.length)];
 
 }
 //Determines if the player or computer wins
 function playRound(playerSelection, computerSelection){
-    if(playerSelection == "rock"){
-        if(computerSelection == "rock"){
-            return "You drew"
+    while(gameRound < 6){
+        if(playerSelection == "rock"){
+         if(computerSelection == "rock"){
+              return "You drew"
+         }
+         else if(computerSelection == "paper"){
+                return "You lost!" + (ComputerPoint += 1);
+            }
+            else{
+                return "You won this round!" + (PlayerPoint += 1);
+         }    
+        } else if(playerSelection == "paper"){
+            if(computerSelection == "rock"){
+             return "You won this round!" + (PlayerPoint += 1);
+         }
+         else if(computerSelection == "paper"){
+             return "You drew!";
+         }
+         else{
+             return "You lost this round" + (ComputerPoint += 1);
+          }    
+         } else if(playerSelection == "scissors"){
+         if(computerSelection == "rock"){
+             return "You lost this round" + (ComputerPoint += 1);
+         }
+         else if(computerSelection == "paper"){
+              return "You won this round!" + (PlayerPoint += 1);
+         }
+         else{
+                return "You drew";
+         }
         }
-        else if(computerSelection == "paper"){
-            return "You lost!";
-        }
-        else{
-            return "You won this round!";
-        }    
-    } else if(playerSelection == "paper"){
-        if(computerSelection == "rock"){
-            return "You won this round!";
-        }
-        else if(computerSelection == "paper"){
-            return "You drew!";
-        }
-        else{
-            return "You lost this round";
-        }    
-    } else if(playerSelection == "scissors"){
-        if(computerSelection == "rock"){
-            return "You lost this round";
-        }
-        else if(computerSelection == "paper"){
-            return "You won this round!";
-        }
-        else{
-            return "You drew";
-        }
+
     }
-
-
 }
 //keeps track of the points for the game
 function game(){
-
+    for(i = gameRound; i < 6; i++){
+        gameRound += 1;
+    }
+}
+//runs the other functions to play the game
+function playGame(){
+    while(game < 6){
+        playRound(getPlayerChoice(), getComputerChoice());
+        console.log(playRound());
+        game();
+    }
+     if(PlayerPoint < ComputerPoint){
+        return "You Lost";
+     } else {
+        return "You won!!";
+     }
+     
 }
