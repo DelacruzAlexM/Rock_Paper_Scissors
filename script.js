53d232420425dfe9dae63ad1b2da63939646d533
DelacruzAlexM <117811408+DelacruzAlexM@users.noreply.github.com>
2023-02-10 00:34:23 -0800

    The game is playable!

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let gameRound = 0;
 
 //get user input
 function getPlayerChoice(){
-    let choice = window.prompt("Rock, Paper, or Scissors?");
-    choice.toLowerCase();
+    let choice = window.prompt("Rock, Paper, or Scissors?").toLowerCase();
     return choice;
 }
 
@@ -23,27 +22,27 @@ function playRound(playerSelection, computerSelection){
               return "You drew"
          }
          else if(computerSelection == "paper"){
-                return "You lost!" + (ComputerPoint += 1);
+                return "You lost!" + (ComputerPoint = ComputerPoint + 1);
             }
             else{
-                return "You won this round!" + (PlayerPoint += 1);
+                return "You won this round!" + (PlayerPoint = PlayerPoint + 1);
          }    
         } else if(playerSelection == "paper"){
             if(computerSelection == "rock"){
-             return "You won this round!" + (PlayerPoint += 1);
+             return "You won this round!" + (PlayerPoint = PlayerPoint + 1);
          }
          else if(computerSelection == "paper"){
              return "You drew!";
          }
          else{
-             return "You lost this round" + (ComputerPoint += 1);
+             return "You lost this round" + (ComputerPoint = ComputerPoint + 1);
           }    
          } else if(playerSelection == "scissors"){
          if(computerSelection == "rock"){
-             return "You lost this round" + (ComputerPoint += 1);
+             return "You lost this round" + (ComputerPoint = ComputerPoint + 1);
          }
          else if(computerSelection == "paper"){
-              return "You won this round!" + (PlayerPoint += 1);
+              return "You won this round!" + (PlayerPoint = PlayerPoint + 1);
          }
          else{
                 return "You drew";
@@ -52,23 +51,20 @@ function playRound(playerSelection, computerSelection){
 
     }
 }
-//keeps track of the points for the game
+//Plays the game and keeps track of the points
 function game(){
-    for(i = gameRound; i < 6; i++){
-        gameRound += 1;
-    }
-}
-//runs the other functions to play the game
-function playGame(){
-    while(game < 6){
+    while(gameRound < 6){   
         playRound(getPlayerChoice(), getComputerChoice());
-        console.log(playRound());
-        game();
+        console.log(playRound);
+        console.log("You have this many points: " + PlayerPoint);
+        console.log("The computer has " + ComputerPoint + " points");
+        gameRound++;
+}
+    if(PlayerPoint > ComputerPoint){
+        return "You have won the game!";
+    } else if(PlayerPoint < ComputerPoint){
+        return "You have lost the game";
+    } else{
+        return "It is a tie";
     }
-     if(PlayerPoint < ComputerPoint){
-        return "You Lost";
-     } else {
-        return "You won!!";
-     }
-     
 }
